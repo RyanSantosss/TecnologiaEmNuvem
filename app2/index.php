dd162f1ba16d9c5eeaf240d8fe34404816babc30
RyanSantosss <161310263+RyanSantosss@users.noreply.github.com>
2025-08-25 10:48:24 -0300

    Create index.php

---
+++ app2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+  <meta charset="UTF-8">
+  <title>Formulário de Contato</title>
+</head>
+<body>
+<h1>Fale Conosco</h1>
+
+  <form method="POST" action"">
+  <label for="nome">Nome:<label><br>
+    <input type="text" name="nome" id"nome" required><br><br>
+
+  <label for="email">Email:<label>
+    
+    
+  </form>
+  
+</body>
