bcf882eb59b960a333f1d0a874e4c78a6aaf6172
RyanSantosss <161310263+RyanSantosss@users.noreply.github.com>
2025-09-01 09:28:17 -0300

    Update index.php

--- app2/index.php
+++ app2/index.php
@@ -12,9 +12,17 @@
   <label for="nome">Nome:<label><br>
     <input type="text" name="nome" id"nome" required><br><br>
 
-  <label for="email">Email:<label>
-    
-    
+  <label for="email">Email:<label><br>
+    <input type="email" name="email" id="email" required><br><br>
+
+    <label for="mensagem">Mensagem:</label><br>
+    <textarea name="mensagem" id="mensagem" rows="5" required></textarea><br><br>
+
+    <input type="submit" value="enviar">  
   </form>
-  
+
+    <?php
+if ($_SERVER["REQ"]
+  ?> 
 </body>
+  
